0cde02e2b893b5c098a9d296b45e56188a23993f
mg.Jung <mg.jung@zeroworks.co.kr>
2026-07-14 15:37:19 +0900

    install launch files in share/launch directory

--- combined_rtk/setup.py
+++ combined_rtk/setup.py
@@ -13,7 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), ['package.xml', *glob.glob('launch/*')]),
+        (os.path.join('share', package_name, 'launch'), glob.glob('launch/*')),
         (os.path.join('share', package_name, 'config'), glob.glob('config/*')),
     ],
     install_requires=['setuptools'],
